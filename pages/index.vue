f2e04e8dd90d910ac0961124d91a19945abca558
Eric Hodges <ehodges2004@gmail.com>
2018-06-12 14:04:14 -0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <main>
+
+  <section>
+        <b-dropdown>
+            <button class="button is-primary" slot="trigger">
+                <span>Click me!</span>
+                <b-icon icon="menu-down"></b-icon>
+            </button>
+
+            <b-dropdown-item>Action</b-dropdown-item>
+            <b-dropdown-item>Another action</b-dropdown-item>
+            <b-dropdown-item>Something else</b-dropdown-item>
+        </b-dropdown>
+
+        <b-dropdown hoverable>
+            <button class="button is-info" slot="trigger">
+                <span>Hover me!</span>
+                <b-icon icon="menu-down"></b-icon>
+            </button>
+
+            <b-dropdown-item>Action</b-dropdown-item>
+            <b-dropdown-item>Another action</b-dropdown-item>
+            <b-dropdown-item>Something else</b-dropdown-item>
+        </b-dropdown>
+
+        <b-dropdown disabled>
+            <button class="button" slot="trigger">
+                <span>Disabled</span>
+                <b-icon icon="menu-down"></b-icon>
+            </button>
+
+            <b-dropdown-item>Action</b-dropdown-item>
+            <b-dropdown-item>Another action</b-dropdown-item>
+            <b-dropdown-item>Something else</b-dropdown-item>
+        </b-dropdown>
+
+        <b-dropdown>
+            <p class="tag is-success" slot="trigger">
+                Custom trigger
+            </p>
+
+            <b-dropdown-item>Action</b-dropdown-item>
+            <b-dropdown-item>Another action</b-dropdown-item>
+            <b-dropdown-item>Something else</b-dropdown-item>
+        </b-dropdown>
+    </section>
+
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        buefy-test
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+  </main>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
