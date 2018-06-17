989a8e83ea38d56b6ece7ed8295f5883457145ec
Eric Hodges <ehodges2004@gmail.com>
2018-06-17 16:03:51 -0700

    test of background gradient animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <main class="main">
 
     <nav class="navbar is-transparent">
       <div class="navbar-brand">
@@ -104,13 +104,37 @@ export default {
   display: flex;
   justify-content: center;
   align-items: top;
-  text-align: center;
-
-  
+  text-align: center;  
 }
-
 
 .links {
   padding-top: 15px;
 }
+
+.main {
+  background: linear-gradient(270deg, #32dcb0, #3257dc);
+  background-size: 400% 400%;
+
+  -webkit-animation: AnimationName 30s ease infinite;
+  -moz-animation: AnimationName 30s ease infinite;
+  animation: AnimationName 30s ease infinite;
+}
+
+
+@-webkit-keyframes AnimationName {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+@-moz-keyframes AnimationName {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+@keyframes AnimationName { 
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+
 </style>
